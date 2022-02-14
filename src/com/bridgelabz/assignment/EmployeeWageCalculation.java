f5cb7e6ca530c993d1f65eacdd6aca097cf9edfe
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 12:26:26 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/assignment/EmployeeWageCalculation.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.assignment;
+
+/**
+ * @author Subhashree Naik
+ *
+ */
+public class EmployeeWageCalculation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee\r\n"
+				+ "Wage Computation\r\n"
+				+ "Program");
+		
+
+	}
+
+}
