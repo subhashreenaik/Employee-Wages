a3e37f053a10ccd6ecbeac9a3975954cc39ebdb5
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 18:49:06 +0530

    Calculating total wages

--- src/com/bridgelabz/assignment/EmployeeWageCalculation.java
+++ src/com/bridgelabz/assignment/EmployeeWageCalculation.java
@@ -10,38 +10,41 @@ public class EmployeeWageCalculation {
 	final static int FULL_TIME=1;
 	final static int PART_TIME=2;
 	final static int WAGE_PER_HOUR=20;
+	final static int WORKINGDAY_MONTH=20;
 	final int FULL_DAY_HOUR=8;
 	final int PART_TIME_HOUR=4;
 	static int working_hour=0;
+	static int total_wage=0;
 	
 	//This method compute daily wage of employee using switchcase
 	static void computeDailyWage() {
+		for(int day=1;day<=WORKINGDAY_MONTH;day++) {
 
         int empType = (int) (Math.random() * 10) % 3;
         switch(empType) {
         case FULL_TIME:
 	      {
-		   System.out.println("Employee is workinf fulltime");
 		   working_hour=8;
 		   break;
 	       }
 	       case PART_TIME:
 	       {
-		   System.out.println("Employee is working parttime");
 		   working_hour=4;
 		   break;
 	
 	       }
 	       default:
 	       {
-		   System.out.println("Employee is absent");
 		   working_hour=0;
 		   break;
 	       }
         }
         
         int wage = working_hour*WAGE_PER_HOUR;
-        System.out.println("Employee Daily Wage is " + wage);
+        total_wage +=wage;
+        
+	}
+		System.out.println("Employee Total Wage is " + total_wage);
 	}
 	
 	
