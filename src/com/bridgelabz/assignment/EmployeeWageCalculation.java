1bf56267ecc8e2716dd6bf85d2162e6c8d787597
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 15:32:35 +0530

    Added:Daily wage calculation program

--- src/com/bridgelabz/assignment/EmployeeWageCalculation.java
+++ src/com/bridgelabz/assignment/EmployeeWageCalculation.java
@@ -6,25 +6,35 @@ package com.bridgelabz.assignment;
  */
 public class EmployeeWageCalculation {
 	
-	//This method checks attendance of employee
-	static void checkEmployee() {
+	//Declaring constant
+	final static int WAGE_PER_HOUR=20;
+	final int FULL_DAY_HOUR=8;
+	
+	static int working_hour=0;
+	
+	//This method compute daily wage of employee
+	static void computeDailyWage() {
 
         int empType = (int) (Math.random() * 10) % 2;
         if (empType == 1)
         {
             System.out.println("Employee is Present");
+            working_hour=8;
 
         } else
         {
             System.out.println("Employee is Absent");
+            working_hour=0;
 
         }
+        int wage = working_hour*WAGE_PER_HOUR;
+        System.out.println("Employee Daily Wage is " + wage);
 	}
 	
 	
 
  public static void main(String[] args) {	
-	      checkEmployee();  	
+	     computeDailyWage();  	
 	        
 	    }
 	}
