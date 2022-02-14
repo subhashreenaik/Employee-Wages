01c3a8b84a8871361b391d17bf79a9ca12fa8af3
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 15:58:43 +0530

    Added:Parttime employee and wage

--- src/com/bridgelabz/assignment/EmployeeWageCalculation.java
+++ src/com/bridgelabz/assignment/EmployeeWageCalculation.java
@@ -7,26 +7,33 @@ package com.bridgelabz.assignment;
 public class EmployeeWageCalculation {
 	
 	//Declaring constant
+	final static int FULL_TIME=1;
+	final static int PART_TIME=2;
 	final static int WAGE_PER_HOUR=20;
 	final int FULL_DAY_HOUR=8;
-	
+	final int PART_TIME_HOUR=4;
 	static int working_hour=0;
 	
 	//This method compute daily wage of employee
 	static void computeDailyWage() {
 
-        int empType = (int) (Math.random() * 10) % 2;
-        if (empType == 1)
+        int empType = (int) (Math.random() * 10) % 3;
+        if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is working fulltime");
             working_hour=8;
 
-        } else
+        }else if (empType == PART_TIME)
         {
-            System.out.println("Employee is Absent");
-            working_hour=0;
+            System.out.println("Employee is working parttime");
+            working_hour=4;
 
         }
+        else
+        {
+        	System.out.println("Employee is absent");
+        }
+        
         int wage = working_hour*WAGE_PER_HOUR;
         System.out.println("Employee Daily Wage is " + wage);
 	}
